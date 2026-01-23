f1d5058e7ea8a4397ab433cfe70630c4471bab92
j9smith <j.smith9103@outlook.com>
2026-01-23 21:20:32 +0000

    Training loop for denoising unet

---
+++ unet/train.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import unet
+from unet import DenoisingUNet
+import time
+
+def step(model, x0, alpha_hats, T, device):
+    batch_size = x0.shape[0]
+    t = torch.randint(low=1, high=T, size=(batch_size,), device=device)
+    alpha_hats = alpha_hats[t].view(batch_size, 1, 1, 1)
+    eps = torch.randn((batch_size, 3, 32, 32), device=device)
+
+    xt = torch.sqrt(alpha_hats) * x0 + torch.sqrt(1 - alpha_hats) * eps
+
+    return eps, model(xt, t)
+
+def train(dataloader, epochs, T, beta_schedule, device):
+    model = DenoisingUNet().to(device)
+    model.train()
+
+    alphas = 1 - beta_schedule
+    alpha_hats = torch.cumprod(alphas, dim=0).to(device)
+
+    loss_fn = nn.MSELoss()
+    optim = torch.optim.AdamW(model.parameters(), lr=2e-4)
+
+    for i in range(epochs):
+        start = time.time()
+
+        epoch_loss = 0
+        batch_count = 0
+
+        for x0, _ in dataloader:
+            x0 = x0.to(device)
+            optim.zero_grad()
+            batch_count += 1
+
+            eps, eps_theta = step(model, x0, alpha_hats, T, device)
+            
+            loss = loss_fn(eps, eps_theta)
+            loss.backward()
+            optim.step()
+
+            epoch_loss += loss.item()
+
+        duration = time.time() - start
+        if i % 10 == 0:
+            print(
+                f"Epoch: {i+1}/{epochs} | "
+                f"Avg. batch loss: {epoch_loss/batch_count:.5f} | "
+                f"Time taken: {duration:.2f}s"
+            )
+
+    torch.save(model.state_dict(), 'ddpm_unet.pt')
