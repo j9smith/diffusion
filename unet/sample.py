0936a2c6fbc045138770f9dc7d5636db1d2095b3
j9smith <j.smith9103@outlook.com>
2026-01-29 19:34:22 +0000

    Add attention for sampling loop, normalise to -1, 1

--- unet/sample.py
+++ unet/sample.py
@@ -4,18 +4,18 @@ from torchvision.utils import make_grid, save_image
 
 if __name__ == '__main__':
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = DenoisingUNet().to(device)
+    model = DenoisingUNet(use_attn=True).to(device)
     model.eval()
 
-    model.load_state_dict(torch.load('ddpm_unet.pt', map_location=device))
+    model.load_state_dict(torch.load('weights/2/ddpm_unet_final.pt', map_location=device))
 
     T = 1000
-    beta_schedule = torch.linspace(start=0.0001, end=0.01, steps=T)
+    beta_schedule = torch.linspace(start=1e-4, end=0.02, steps=T, device=device)
 
     samples = model.sample(device, beta_schedule)
-    print(samples.min().item(), samples.max().item(), samples.mean().item(), samples.std().item())
 
-    grid = make_grid(samples, nrow=6, normalize=True)
+    #samples = samples * 0.5 + 0.5
+    grid = make_grid(samples, nrow=6, normalize=True, value_range=(-1, 1))
     save_image(grid, 'samples.png')
 
 
