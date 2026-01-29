c1bbdd33d647dc9ef66b4713a48d1fd90588f4fe
j9smith <j.smith9103@outlook.com>
2026-01-29 19:33:23 +0000

    Normalise cifar to -1, 1

--- unet/cifar10.py
+++ unet/cifar10.py
@@ -1,13 +1,12 @@
 import train
-
 import torch
 from torch.utils.data import DataLoader, Subset
 from torchvision import datasets, transforms
 
-
 if __name__ == '__main__':
     train_tf = transforms.Compose([
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
     train_ds = datasets.CIFAR10(
@@ -18,13 +17,14 @@ if __name__ == '__main__':
     )
 
     dog_idxs = [i for i, target in enumerate(train_ds.targets) if target == 5] # Select only dogs
+    #dog_idxs = dog_idxs[:128]
     dog_ds = Subset(train_ds, dog_idxs)
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    loader = DataLoader(dog_ds, batch_size=32, shuffle=True)
+    loader = DataLoader(dog_ds, batch_size=64, shuffle=True)
 
-    epochs = 1000
+    epochs = 5000
     T = 1000
-    beta_schedule = torch.linspace(start=0.0001, end=0.01, steps=T)
+    beta_schedule = torch.linspace(start=1e-4, end=0.02, steps=T).to(device)
 
     train.train(loader, epochs, T, beta_schedule, device)
